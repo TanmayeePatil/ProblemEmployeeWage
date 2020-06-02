a768bf65a76117b8790437e50845f75a1ef406f9
TanmayeePatil <tanmayeepatil@outlook.com>
2020-06-03 05:29:32 +0530

    Add: Solved Eamployee Wage Problem Using Switch Case

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -5,19 +5,25 @@ echo "Welcome to Employee Wage Computation Program"
 empWagePerHrs=20
 empSalary=0
 
+IS_FULL_TIME=1
+IS_PART_TIME=2
+
 function ChechEmpWage()
 {
-	myRan=$((RANDOM%3))
-	if [ $myRan -eq 1 ];then
-	   echo "Employee is present: Full Time"
-           empHrs=8
-	elif [ $myRan -eq 2 ];then
-	   echo "Employee is present: Part Time"
-	   empHrs=4
-        else
-           echo "Employee is absent"
-           empHrs=0
-	fi
+        myRan=$((RANDOM%3))
+        case $myRan in
+		$IS_FULL_TIME)
+	   		echo "Employee is present: Full Time"
+           		empHrs=8
+			;;
+	        $IS_PART_TIME)
+	   		echo "Employee is present: Part Time"
+	   		empHrs=4
+        		;;
+                *)
+                        echo "Employee is absent"
+                        empHrs=0
+	esac
 }
 
 echo "Check Attendance:"
