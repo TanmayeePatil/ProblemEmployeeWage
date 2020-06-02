3f321d34085499c43de098d260fb52e7a88dcb6d
TanmayeePatil <tanmayeepatil@outlook.com>
2020-06-02 22:34:16 +0530

    Add: Added Attendance Functionality

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
 echo "Welcome to Employee Wage Computation Program"
+
+function CheckAttendance()
+{
+	myRan=$((RANDOM%2))
+	if [ $myRan -eq 1 ];then
+	   echo "Employee is present"
+	else
+	   echo "Employee is absent"
+	fi
+}
+
+echo "Check Attendance:"
+CheckAttendance
+
