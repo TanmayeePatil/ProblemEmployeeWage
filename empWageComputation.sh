06f76abf769d7e9bacc81e746dbd3f4dab2b205d
TanmayeePatil <tanmayeepatil@outlook.com>
2020-06-03 06:38:18 +0530

    Add: Calculated Employee Wage  for (( i=1; i<=;i++ ))

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -10,35 +10,35 @@ IS_FULL_TIME=1
 IS_PART_TIME=2
 DaysPerMonth=20
 
+TotalHr=0
+TotalMaxHr=100
+
 function ChechEmpWage()
 {
- for (( i=1; i<=$DaysPerMonth;i++ ))
- do
+  while [ $TotalHr -le $TotalMaxHr ]
+  do
         myRan=$((RANDOM%3))
         case $myRan in
 		$IS_FULL_TIME)
-	   		echo -e "\nEmployee is present: Full Time"
            		empHrs=8
 			;;
 	        $IS_PART_TIME)
-	   		echo -e "\nEmployee is present: Part Time"
 	   		empHrs=4
         		;;
                 *)
-                        echo -e "\nEmployee is absent"
                         empHrs=0
 	esac
 
-        empSalary=$(( $empWagePerHrs * $empHrs ))
-        echo "Day $i : Salary of the employee: $empSalary"
+	TotalHr=$(( $TotalHr + $empHrs ))
+        empSalary=$(( $empWagePerHrs * $TotalHr ))
         TotalEmpSalary=$(( $empSalary + $TotalEmpSalary))
  done
 }
 
-echo "Check Attendance:"
+#echo "Check Attendance:"
 ChechEmpWage
 
 #Calculating Salary
 empSalary=$(( $empWagePerHrs * $empHrs ))
-echo "----------------------------------------------"
-echo -e "\nTotal Monthly Salary: $TotalEmpSalary"
+#echo "----------------------------------------------"
+echo -e "\nTotal Salary for 100hr: $TotalEmpSalary"
