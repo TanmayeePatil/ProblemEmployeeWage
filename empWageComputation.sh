7151c87dbdc9485f0072e005598eef5ff44346e7
TanmayeePatil <tanmayeepatil@outlook.com>
2020-06-03 05:44:54 +0530

    Add: Calculated monthly salary of the employee

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -4,26 +4,35 @@ echo "Welcome to Employee Wage Computation Program"
 
 empWagePerHrs=20
 empSalary=0
+TotalEmpSalary=0
 
 IS_FULL_TIME=1
 IS_PART_TIME=2
+DaysPerMonth=20
 
 function ChechEmpWage()
 {
+ for (( i=1; i<=$DaysPerMonth;i++ ))
+ do
         myRan=$((RANDOM%3))
         case $myRan in
 		$IS_FULL_TIME)
-	   		echo "Employee is present: Full Time"
+	   		echo -e "\nEmployee is present: Full Time"
            		empHrs=8
 			;;
 	        $IS_PART_TIME)
-	   		echo "Employee is present: Part Time"
+	   		echo -e "\nEmployee is present: Part Time"
 	   		empHrs=4
         		;;
                 *)
-                        echo "Employee is absent"
+                        echo -e "\nEmployee is absent"
                         empHrs=0
 	esac
+
+        empSalary=$(( $empWagePerHrs * $empHrs ))
+        echo "Day $i : Salary of the employee: $empSalary"
+        TotalEmpSalary=$(( $empSalary + $TotalEmpSalary))
+ done
 }
 
 echo "Check Attendance:"
@@ -31,5 +40,5 @@ ChechEmpWage
 
 #Calculating Salary
 empSalary=$(( $empWagePerHrs * $empHrs ))
-
-echo "Salary of the employee: $empSalary"
+echo "----------------------------------------------"
+echo -e "\nTotal Monthly Salary: $TotalEmpSalary"
